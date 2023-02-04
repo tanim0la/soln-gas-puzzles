3550179474866d114f244670e80f988df956372d
Tanim0la <36541366+tanim0la@users.noreply.github.com>
2023-02-05 00:38:14 +0100

    Update OptimizedMint150.sol

--- contracts/contracts_optimized/OptimizedMint150.sol
+++ contracts/contracts_optimized/OptimizedMint150.sol
@@ -20,17 +20,35 @@ contract NotRareToken is ERC721 {
 
 contract OptimizedAttacker {
     constructor(address victim) {
-        address attacker = msg.sender;
         NotRareToken vic = NotRareToken(victim);
         unchecked {
             uint256 start = vic.balanceOf(vic.ownerOf(1)) + 1;
             uint256 len = 150 + start;
-            vic.mint();
-            for (uint256 i = start + 1; i < len; i++) {
-                vic.mint();
-                vic.transferFrom(address(this), attacker, i);
+            assembly {
+                // stores function selector "mint()" in memory postion 0x00
+                mstore(
+                    0x00,
+                    0x1249c58b00000000000000000000000000000000000000000000000000000000
+                )
+
+                /* 
+                    `call` opcode returns a value that get pop out of the stack using the
+                    `pop` opcode
+                */
+                pop(call(gas(), vic, 0, 0x00, 0x04, 0, 0))
             }
-            vic.transferFrom(address(this), attacker, start);
+
+            for (uint256 i = start + 1; i < len; i++) {
+                assembly {
+                    /* 
+                        `call` opcode returns a value that get pop out of the stack using the
+                        `pop` opcode
+                    */
+                    pop(call(gas(), vic, 0, 0x00, 0x04, 0, 0))
+                }
+                vic.transferFrom(address(this), msg.sender, i);
+            }
+            vic.transferFrom(address(this), msg.sender, start);
         }
     }
 }
